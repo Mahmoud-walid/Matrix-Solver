d040868a377c36c68533e36c2c8f285e416ad7fd
mahmoud_walid <modywmbadr@gmail.com>
2023-10-27 18:28:59 +0200

    Matrix processing

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,9 @@
 
 const userInputsUl = document.querySelector(".App .userInputs ul");
 const numMatInputs = document.querySelector(".inputNumRows");
-const generateMarkupInputs = (N = 2) => {
+const generateMarkupInputs = (N = +numMatInputs.value) => {
   try {
-    if (N <= 1) throw Error(`can't create matrix with ${N} Rows`);
+    // if (N <= 1) throw Error(`can't create matrix with ${N} Rows`);
     userInputsUl.innerHTML = ``;
 
     for (let i = 0; i < N; i++) {
@@ -14,27 +14,19 @@ const generateMarkupInputs = (N = 2) => {
         const input = document.createElement("input");
         li.appendChild(input);
         input.classList.add(`inputMat`, `column${j + 1}`, `Row${i + 1}`);
+        input.setAttribute("placeholder", `r${i + 1}c${j + 1}`);
         input.setAttribute("type", "text");
       }
       userInputsUl.appendChild(li);
     }
     console.log(userInputsUl);
+    return { N };
   } catch (error) {
     console.error(error);
     userInputsUl.innerHTML = error.message;
   }
 };
 generateMarkupInputs(2);
-
-const addElementsToMatrix = () => {
-  const arr = new Array(+numMatInputs.value * (+numMatInputs.value + 1));
-  document.querySelectorAll("input.inputMat").forEach(ele => {
-    for (let i = 0; i < arr.length; i++) {
-      arr[i] += +ele.value;
-    }
-  });
-  console.log(arr);
-};
 
 userInputsUl.addEventListener("keydown", event => {
   // prettier-ignore
@@ -47,11 +39,68 @@ userInputsUl.addEventListener("keydown", event => {
 });
 
 numMatInputs.addEventListener("keydown", event => {
-  // prettier-ignore
-  if ((event.key === "Enter" || /^[0-9]$/.test(event.key)) && !event.ctrlKey && !event.altKey) {
-    if (event.key === "Enter") {
+  try {
+    // prettier-ignore
+    if ((event.key === "Enter" || /^[0-9]$/.test(event.key)) && !event.ctrlKey && !event.altKey) {
+    if (event.key === "Enter" || event.key === "Tab") {
     event.preventDefault();
       generateMarkupInputs(+numMatInputs.value);
+      if (isNaN(generateMarkupInputs().N)) throw Error(`"${numMatInputs.value}" isn't a number`)
+      if (generateMarkupInputs().N <= 1) throw Error(`can't create matrix with "${generateMarkupInputs().N}" Rows`);
+    }
+  }
+  } catch (error) {
+    console.error(error);
+    userInputsUl.innerHTML = error.message;
+  }
+});
+
+// ⭐
+const addElementsToMatrix = () => {
+  const N = +numMatInputs.value;
+  let arr = [];
+  let row = [];
+  document.querySelectorAll("input.inputMat").forEach((ele, index) => {
+    row.push(+ele.value);
+    if ((index + 1) % (N + 1) === 0) {
+      arr.push(row);
+      row = [];
+    }
+  });
+  console.log(arr);
+  return arr;
+};
+
+//
+//
+//
+const zoomInButton = document.getElementById("zoomInButton");
+const zoomOutButton = document.getElementById("zoomOutButton");
+
+zoomInButton.addEventListener("click", () => {
+  zoomIn();
+});
+
+zoomOutButton.addEventListener("click", () => {
+  zoomOut();
+});
+
+function zoomIn() {
+  document.body.style.zoom =
+    parseFloat(getComputedStyle(document.body).zoom) + 0.1;
+}
+
+function zoomOut() {
+  document.body.style.zoom =
+    parseFloat(getComputedStyle(document.body).zoom) - 0.1;
+}
+
+document.addEventListener("keydown", e => {
+  if (e.ctrlKey) {
+    if (e.deltaY < 0) {
+      zoomIn();
+    } else if (e.deltaY > 0) {
+      zoomOut();
     }
   }
 });
