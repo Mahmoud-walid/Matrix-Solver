d18a8da8871471912215708804b973ab13714111
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-11-28 02:18:07 +0200

    fix TTS

--- src/js/view/TTSView.js
+++ src/js/view/TTSView.js
@@ -3,18 +3,28 @@ import { app } from "../helpers.js";
 const TTSView = function () {};
 
 TTSView.prototype.readSelectedText = function () {
+  let selectedVoice = speechSynthesis.getVoices()[3];
+  let lastSelectedText = "";
+
   app.addEventListener("select", function () {
     setTimeout(() => {
-      speechSynthesis.cancel();
+      if (getSelectedText() !== lastSelectedText) {
+        lastSelectedText = getSelectedText();
+        speechSynthesis.cancel();
 
-      if (getSelectedText() !== "") {
-        const speech = new SpeechSynthesisUtterance();
-        speech.text = getSelectedText();
-        speech.voice = speechSynthesis.getVoices()[0];
-        speechSynthesis.speak(speech);
+        if (getSelectedText() !== "") {
+          const speech = new SpeechSynthesisUtterance();
+          speech.text = getSelectedText();
+          speech.voice = selectedVoice;
+          speechSynthesis.speak(speech);
+        }
       }
     }, 1000);
   });
+
+  speechSynthesis.onvoiceschanged = function () {
+    selectedVoice = speechSynthesis.getVoices()[3];
+  };
 
   const getSelectedText = () => {
     let text = "";
